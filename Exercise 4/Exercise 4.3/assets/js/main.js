3b67f341d1d3faa9d62db03d74a280747b051e4e
106212966 <106212966@student.swin.edu.au>
2026-09-12 20:28:00 +0800

    Update latest version of index.html
    Update the path of getting the main.js, change the bunch of code on the top.

--- Exercise 4/Exercise 4.3/assets/js/main.js
+++ Exercise 4/Exercise 4.3/assets/js/main.js
@@ -1,11 +1,13 @@
+// Exercise 4.2 Step 2
 d3.select("h1")
   .style("color", "green");
 
-
-d3.select("div.container")
+// Exercise 4.2 Step 3
+d3.select("div")
   .append("p")
   .text("Purchasing a low energy consumption TV will help with your energy bills!");
 
+// Exercise 4.2 Step 4
 d3.select("svg")
   .append("rect")
   .attr("x", 50)
